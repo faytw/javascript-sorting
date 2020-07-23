37ea437c1c0edd79744c252f74a87d6b9dad6cf9
Fay <rebecca.hsu1989@gmail.com>
2020-07-23 15:44:09 +0800

    add comment

--- mergeSort.js
+++ mergeSort.js
@@ -34,15 +34,15 @@ const  merge = (left, right) => {
 const cutArray = (array) => {
   const length = array.length
   if (length <= 1) return array
-
   const middle = Math.floor(length / 2)
+  // 先切一半
   const left = array.slice(0, middle)
   const right = array.slice(middle, length)
-
+  // 將兩邊分別再切一半，排序
   const sortLeftArray = cutArray(left)
   const sortRightArray = cutArray(right)
-
+  // 合併
   return merge(sortLeftArray, sortRightArray)
 }
 
-console.log(mergeSort(random)) // [0, 1, 2, 5, 7, 11, 23, 80, 99]
+console.log(mergeSort(random)) // [1, 2, 5, 7, 11, 23, 80, 99]
